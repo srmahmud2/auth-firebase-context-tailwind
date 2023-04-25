512ae4c824129a623d2dace1fedd7050a4cb0494
srmahmud2 <srmahmud2@gmail.com>
2023-04-25 15:03:43 +0100

    ignore ignore

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxPMlXajdiVnyPirYi--BGV9VF8FL1Ocw",
+  authDomain: "auth-firebase-context-tailwing.firebaseapp.com",
+  projectId: "auth-firebase-context-tailwing",
+  storageBucket: "auth-firebase-context-tailwing.appspot.com",
+  messagingSenderId: "636345737400",
+  appId: "1:636345737400:web:317f4bf499fc494f38944d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
